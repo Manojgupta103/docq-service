baac1acb94ebbfaa438f89585747ffca4166de88
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-09 00:44:15 +0530

    Optimize image loading and update styling in Diagnostic and Consultation pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Foot from "@/components/Foot";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <Foot />
+        <Header />
+        <Footer />
       </body>
     </html>
   );
