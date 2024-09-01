a424bf822be613284a31b397b9af2b8e4ec56e01
Manoj <manoj.gupta16539@sakec.ac.in>
2024-09-02 01:31:02 +0530

    chore: Optimize image loading in Service component

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 import consultation from "../../../public/consultation.png";
 import Diagnostic from "../../../public/Diagnostic.png";
@@ -25,15 +25,46 @@ const ServiceComponent = () => {
             We are Here for You, Every Step of the Way
           </h1>
           <h3 className="text-[16px] lg:text-[18px]">
-            From everyday care to those moments you need a specialist, our services are here to support you
+            From everyday care to those moments you need a specialist, our
+            services are here to support you
           </h3>
         </div>
         <div className="relative mt-10 lg:mt-0">
-          <Image src={Ellipse} alt="Ellipse" width={500} height={500} className="rounded-lg max-w-full" />
-          <Image src={Rectangle126} alt="Rectangle 134" width={450} height={450} className="absolute -top-28 right-3 max-w-full" />
-          <Image src={image} alt="Rectangle 135" width={150} height={150} className="absolute top-0 left-0 max-w-full" />
-          <Image src={imaged} alt="Rectangle 135" width={150} height={150} className="absolute bottom-0 -left-14 max-w-full" />
-          <Image src={imageo} alt="Rectangle 135" width={150} height={150} className="absolute top-28 -right-16 max-w-full" />
+          <Image
+            src={Ellipse}
+            alt="Ellipse"
+            width={500}
+            height={500}
+            className="rounded-lg max-w-full"
+          />
+          <Image
+            src={Rectangle126}
+            alt="Rectangle 134"
+            width={450}
+            height={450}
+            className="absolute -top-28 right-3 max-w-full"
+          />
+          <Image
+            src={image}
+            alt="Rectangle 135"
+            width={150}
+            height={150}
+            className="absolute top-0 left-0 max-w-full"
+          />
+          <Image
+            src={imaged}
+            alt="Rectangle 135"
+            width={150}
+            height={150}
+            className="absolute bottom-0 -left-14 max-w-full"
+          />
+          <Image
+            src={imageo}
+            alt="Rectangle 135"
+            width={150}
+            height={150}
+            className="absolute top-28 -right-16 max-w-full"
+          />
         </div>
       </div>
 
@@ -44,26 +75,44 @@ const ServiceComponent = () => {
         </h2>
         <div className="bg-[#dde7ff] p-4 lg:p-8 rounded-[30px] lg:rounded-[60px] flex flex-col lg:flex-row justify-center gap-8 w-full lg:w-[70%] mx-auto">
           <div className="flex flex-col items-center bg-white rounded-3xl shadow-lg transition-shadow duration-300 overflow-hidden w-full lg:w-[250px] h-[350px]">
-            <Image src={consultation} alt="Consultation" className="object-cover w-full h-[60%]" />
+            <Image
+              src={consultation}
+              alt="Consultation"
+              className="object-cover w-full h-[60%]"
+            />
             <div className="p-6 text-center hover:bg-blue-500 hover:text-white rounded-b-3xl transition-colors duration-300 w-full">
               <h3 className="text-lg font-medium mt-4">Consultation</h3>
-              <button className="bg-[#00CFFF] text-white py-2 px-4 rounded-full mt-4">Book Now</button>
+              <button className="bg-[#00CFFF] text-white py-2 px-4 rounded-full mt-4">
+                Book Now
+              </button>
             </div>
           </div>
 
           <div className="flex flex-col items-center bg-white rounded-3xl shadow-lg transition-shadow duration-300 overflow-hidden w-full lg:w-[250px] h-[350px]">
-            <Image src={Diagnostic} alt="Diagnostic Center" className="object-cover w-full h-[60%]" />
+            <Image
+              src={Diagnostic}
+              alt="Diagnostic Center"
+              className="object-cover w-full h-[60%]"
+            />
             <div className="p-6 text-center hover:bg-blue-500 hover:text-white rounded-b-3xl transition-colors duration-300 w-full">
               <h3 className="text-lg font-medium mt-4">Diagnostic Center</h3>
-              <button className="bg-[#00CFFF] text-white py-2 px-4 rounded-full mt-4">Book Now</button>
+              <button className="bg-[#00CFFF] text-white py-2 px-4 rounded-full mt-4">
+                Book Now
+              </button>
             </div>
           </div>
 
           <div className="flex flex-col items-center bg-white rounded-3xl shadow-lg transition-shadow duration-300 overflow-hidden w-full lg:w-[250px] h-[350px]">
-            <Image src={operation} alt="Operation Center" className="object-cover w-full h-[60%]" />
+            <Image
+              src={operation}
+              alt="Operation Center"
+              className="object-cover w-full h-[60%]"
+            />
             <div className="p-6 text-center hover:bg-blue-500 hover:text-white rounded-b-3xl transition-colors duration-300 w-full">
               <h3 className="text-lg font-medium mt-4">Operation Center</h3>
-              <button className="bg-[#00CFFF] text-white py-2 px-4 rounded-full mt-4">Book Now</button>
+              <button className="bg-[#00CFFF] text-white py-2 px-4 rounded-full mt-4">
+                Book Now
+              </button>
             </div>
           </div>
         </div>
@@ -78,12 +127,17 @@ const ServiceComponent = () => {
           3 Easy steps to your service
         </p>
         <div className="flex flex-col items-center w-full">
-          <Image src={stepsImage} alt="How it Works Steps" className="w-full max-w-[750px] mb-8" />
+          <Image
+            src={stepsImage}
+            alt="How it Works Steps"
+            className="w-full max-w-[750px] mb-8"
+          />
           <div className="flex flex-col lg:flex-row justify-around w-full max-w-[800px] text-center">
             <div className="lg:w-1/3">
               <h3 className="text-lg font-bold">Pick a Service</h3>
               <p className="text-sm text-gray-600">
-                Find the right care for your needs from our comprehensive range of services.
+                Find the right care for your needs from our comprehensive range
+                of services.
               </p>
             </div>
             <div className="lg:w-1/3 mt-4 lg:mt-0">
@@ -112,7 +166,8 @@ const ServiceComponent = () => {
               </div>
             </div>
             <h1 className="text-[24px] lg:text-[32px] font-bold mb-2">
-              We're the Right Choice <br className="hidden lg:block" /> for Your Healthcare
+              We're the Right Choice <br className="hidden lg:block" /> for Your
+              Healthcare
             </h1>
             <p className="text-base lg:text-lg text-black mb-4">
               We’re here to make your healthcare experience easy, <br />
@@ -120,27 +175,69 @@ const ServiceComponent = () => {
             </p>
             <ul className="space-y-4 text-base lg:text-lg">
               <li className="flex items-start">
-                <span className="text-[24px] lg:text-[32px] font-bold mb-2">●</span> 
-                <h1 className="text-[20px] lg:text-[28px] font-bold mb-2"> Whatever you need, we’ve got it covered, from check-ups to specialized treatments.</h1>
+                <span className="text-[24px] lg:text-[32px] font-bold mb-2">
+                  ●
+                </span>
+                <h1 className="text-[20px] lg:text-[28px] font-bold mb-2">
+                  {" "}
+                  Whatever you need, we’ve got it covered, from check-ups to
+                  specialized treatments.
+                </h1>
               </li>
               <li className="flex items-start">
-                <span className="text-[24px] lg:text-[32px] font-bold mb-2">●</span> 
-                <h1 className="text-[20px] lg:text-[28px] font-bold mb-2"> We listen, we care, and we tailor everything to what works best for you.</h1>
+                <span className="text-[24px] lg:text-[32px] font-bold mb-2">
+                  ●
+                </span>
+                <h1 className="text-[20px] lg:text-[28px] font-bold mb-2">
+                  {" "}
+                  We listen, we care, and we tailor everything to what works
+                  best for you.
+                </h1>
               </li>
             </ul>
           </div>
           <div className="relative mt-10 lg:mt-0">
-            <Image src={Group7} alt="Doctor pointing" width={400} height={400} className="rounded-lg max-w-full" />
-            <Image src={Rectangle134} alt="Rectangle 134" width={400} height={400} className="absolute top-0 right-3 max-w-full" />
-            <Image src={mdl2} alt="Rectangle 135" width={100} height={100} className="absolute top-0 left-0 max-w-full" />
-            <Image src={plus} alt="Rectangle 135" width={100} height={100} className="absolute bottom-0 -left-14 max-w-full" />
+            <Image
+              src={Group7}
+              alt="Doctor pointing"
+              width={400}
+              height={400}
+              className="rounded-lg max-w-full"
+            />
+            <Image
+              src={Rectangle134}
+              alt="Rectangle 134"
+              width={400}
+              height={400}
+              className="absolute top-0 right-3 max-w-full"
+            />
+            <Image
+              src={mdl2}
+              alt="Rectangle 135"
+              width={100}
+              height={100}
+              className="absolute top-0 left-0 max-w-full"
+            />
+            <Image
+              src={plus}
+              alt="Rectangle 135"
+              width={100}
+              height={100}
+              className="absolute bottom-0 -left-14 max-w-full"
+            />
           </div>
         </div>
       </div>
 
       {/* Final Image Section */}
       <div className="flex justify-center items-center w-full p-4">
-        <Image src={Group345} alt="Group" width={800} height={800} className="max-w-full" />
+        <Image
+          src={Group345}
+          alt="Group"
+          width={800}
+          height={800}
+          className="max-w-full"
+        />
       </div>
     </>
   );
