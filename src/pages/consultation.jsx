78d17a37d034b3c1aa7d1f478329167727f75285
Manoj <manoj.gupta16539@sakec.ac.in>
2024-09-05 20:45:04 +0530

    Optimize image loading and update styling in Consultation page

--- src/pages/consultation.jsx
+++ src/pages/consultation.jsx
@@ -11,9 +11,7 @@ const ConsultationPage = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Consultation Services</h1>
       <p>
-        Detailed information about our consultation services will be available
-        
-        here.
+        Detailed information about our consultation services will be available here.
       </p>
     </div>
     <Foot/>
